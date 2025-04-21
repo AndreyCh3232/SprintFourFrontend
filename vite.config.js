00eecb4583ce6fee3cc38dceeb1ed45c0dd83d2c
Andrey Chetvergov <andrey32102@gmail.com>
2025-04-21 16:33:51 +0300

    Fix the code

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/SprintFourFrontend/',
 	plugins: [react()],
-	// build: {
-	// 	outDir: '../backend/public',
-	// 	emptyOutDir: true,
-	// },
-	// css: {
-	// 	devSourcemap: true
-	// }
+	build: {
+		outDir: '../backend/public',
+		emptyOutDir: true,
+	},
+	css: {
+		devSourcemap: true
+	}
 })
